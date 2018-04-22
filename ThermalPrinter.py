2268c7a65500d020c74d01ee627348877a2f7c28
Giorgos Karatziolas <gkaratziolas@gmail.com>
2018-04-22 21:54:15 +0100

    Printer works for printing chars, but leads with two unexpected chars

--- ThermalPrinter.py
+++ ThermalPrinter.py
@@ -1,5 +1,18 @@
+import time
+import serial
+
+# ASCII codes used by some of the printer config commands:
+ASCII_TAB = '\t' # Horizontal tab
+ASCII_LF  = '\n' # Line feed
+ASCII_FF  = '\f' # Form feed
+ASCII_CR  = '\r' # Carriage return
+ASCII_DC2 =  18  # Device control 2
+ASCII_ESC =  27  # Escape
+ASCII_FS  =  28  # Field separator
+ASCII_GS  =  29  # Group separator
+
 kPrinterBaudrate = 19200
-kPrinterTimeout  = 2
+kPrinterTimeout  = 20
 
 # use chr to convert to either dec or hex to printable ascii chars
 
@@ -27,20 +40,103 @@ kHeatTime     = 44
 kHeatDots     = 80
 kHeatInterval = 20
 
-###begin
-#timeoutSet(500000L);
-#wake();
-#reset();
-#writeBytes(ASCII_ESC, '7');   // Esc 7 (print settings)
-#writeBytes(44, heatTime, 20); // Heating dots, heat time, heat interval
+
+  # Print density description from manual:
+  # DC2 # n Set printing density
+  # D4..D0 of n is used to set the printing density.  Density is
+  # 50% + 5% * n(D4-D0) printing density.
+  # D7..D5 of n is used to set the printing break time.  Break time
+  # is n(D7-D5)*250us.
+  # (Unsure of the default value for either -- not documented)
+
+kPrintDensity   = 10 # 100% (? can go higher, text is darker but fuzzy)
+kPrintBreakTime = 2 # 500 uS
+
+
+kDotPrintTime   = 30000; # See comments near top of file for
+kDotFeedTime    =  2100; # an explanation of these values.
+kMaxChunkHeight =   255;
+
+
 
 class ThermalPrinter(object):
-	def __init__(self, port):
-		printer.ser = serial.Serial(port, baudrate=kPrinterBaudrate, timeout=kPrinterTimeout)
+    def __init__(self, port):
+        self.ser = serial.Serial(port, baudrate=kPrinterBaudrate, timeout=kPrinterTimeout)
 
-	def send_byte(b):
-		if type(b) == bytes:
+        self.previous_byte = ""
+        self.column        = 0
+        self.maxColumn     = 0
+        self.charHeight    = 0
+        self.lineSpacing   = 0
+        self.barcodeHeight = 0
 
-		elif type(b) == str:
+        self.wake()
+        self.reset()
+        self.send_bytes(ASCII_ESC, '7');   # Esc 7 (print settings)
+        self.send_bytes(kHeatDots, kHeatTime, kHeatInterval); # Heating dots, heat time, heat interval
+        self.send_bytes(ASCII_DC2, '#', (kPrintBreakTime << 5) | kPrintDensity);
 
-		elif type(b) == hex:
+        self.previous_byte = "\n"
+
+    def send_byte(self, b):
+        if type(b) == bytes:
+            data = b
+        elif type(b) == str:
+            data = b
+        elif type(b) == hex:
+            data = chr(b)
+        elif type(b) == float:
+            b = int(b)
+        elif type(b) == int:
+            data = chr(b)
+
+        data = data.encode("utf-8")
+
+        self.ser.write(data)
+        print(data)
+
+    def send_bytes(self, *bs):
+        for b in bs:
+            if type(b) == str:
+                for c in list(b):
+                    self.send_byte(c)
+            else:
+                self.send_byte(b)
+
+    def wake(self):
+        self.send_bytes(255) # Wake from low-energy state
+        time.sleep(0.05)
+        self.send_bytes(ASCII_ESC, '8', 0, 0) # Sleep off
+
+    def reset(self):
+        self.send_bytes(ASCII_ESC, "@") # Init command
+
+        self.previous_byte = "\n"       # Treat as if prior line is blank
+        self.column        = 0
+        self.maxColumn     = 32
+        self.charHeight    = 24
+        self.lineSpacing   = 6
+        self.barcodeHeight = 50
+
+        # Configure tab stops
+        self.send_bytes(ASCII_ESC, 'D') # Set tab stops...
+        self.send_bytes( 4,  8, 12, 16) # ...every 4 columns,
+        self.send_bytes(20, 24, 28,  0) # 0 marks end-of-list.
+
+    def feed(self, n):
+        self.send_bytes(ASCII_ESC, 'd', n)
+        #timeoutSet(dotFeedTime * charHeight)
+        self.prevByte = '\n';
+        self.column   =    0;
+
+    def flush(self):
+        self.send_bytes(ASCII_FF)
+
+
+if __name__ == "__main__":
+    printer = ThermalPrinter("/dev/ttyUSB0")
+    printer.feed(1)
+    #printer.flush()
+    printer.send_bytes("Hello World\nHello\nHello\n")
+    printer.send_bytes("AAA")
+    printer.feed(1)
